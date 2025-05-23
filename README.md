456378ff764224feca30607d01ea741f7312764b
Jaime Hernandez <jaimevazz@gmail.com>
2025-05-22 23:55:52 -0400

    Add sender and receiver in /bin

---
+++ README.md
@@ -0,0 +1,21 @@
+# Simple UDP
+
+Simple but complete Rust implementation with both the UDP sender (server) and receiver (client).
+
+## How to run
+
+### Receiver
+
+First, run the receiver in one terminal:
+
+```sh
+cargo run --bin receiver
+```
+
+### Sender
+
+Then, in another terminal, run the sender:
+
+```sh
+cargo run --bin sender
+```
---
+++ src/bin/receiver.rs
@@ -0,0 +1,25 @@
+use std::net::UdpSocket;
+use std::time::SystemTime;
+
+fn main() -> std::io::Result<()> {
+    // Bind to the port we want to receive on
+    let socket = UdpSocket::bind("127.0.0.1:34254")?;
+    println!("UDP receiver running on port 34254");
+    
+    let mut buf = [0; 1]; // Buffer for 1 byte (u8)
+    
+    loop {
+        // Receive data
+        let (amt, _src) = socket.recv_from(&mut buf)?;
+        
+        if amt == 1 {
+            let num = buf[0];
+            let timestamp = SystemTime::now()
+                .duration_since(SystemTime::UNIX_EPOCH)
+                .unwrap()
+                .as_secs_f64();
+            
+            println!("{:.6}: Received number: {}", timestamp, num);
+        }
+    }
+}
